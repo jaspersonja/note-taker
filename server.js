b6063ec60fe0d2353b3b88938458ec728a5122af
unknown <jacob.jasperson@yahoo.com>
2022-11-29 23:46:33 -0700

    built routes and server js files

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+require('./routes/routes')(app);
+
+app.listen(PORT, () => 
+    console.log(`App listening at PORT: ${PORT}`)
+);
